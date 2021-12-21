d5595f16008727662420cb13c01b2c6b8bba0c27
Ben Kasper <bckasper@gmail.com>
2021-12-21 09:36:12 -0600

    added content and formatting to the nav header

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,35 @@ import React from 'react'
 
 const Header = () => {
     return(
-        <div>
-            <p className="text-primary">This is my header</p>
-        </div>
+        <ul className='nav nav-tabs'>
+            <li className="nav-item">
+        <a
+          href="#about"
+        //   onClick={() => changePageHandler('About')}
+        //   className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#portfolio"
+        //   onClick={() => handlePageChange('Portfolio')}
+        //   className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+        //   onClick={() => handlePageChange('Contact')}
+        //   className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+        </ul>
     )
 }
 
