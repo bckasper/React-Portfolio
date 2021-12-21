fd22810604a2045285d611059e52fcc6102375af
Ben Kasper <bckasper@gmail.com>
2021-12-21 10:44:53 -0600

    finished the about me page by adding the propoer CSS to bring in a background image

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -3,12 +3,14 @@ import React from 'react';
 export default function About() {
   return (
     <div className='about-me'>
-      <h1>About Page</h1>
-      <p>
-        I am a freelance developer that enjoys building beautiful web applications, primarily through JavaScript, HTML, and CSS. My applications are often inspired by my love for travel, food and stunning natural wonders. I have been fortunate enough to have a career that has allowed me to pursue these passions and am always looking for ways to collaborate with new people who are driven to pursue theirs with me. 
+      <div>
+        <h1>About Me</h1>
+        <p>
+          I am a freelance developer that enjoys building beautiful web applications, primarily through JavaScript, HTML, and CSS. My applications are often inspired by my love for travel, food and stunning natural wonders. I have been fortunate enough to have a career that has allowed me to pursue these passions and am always looking for ways to collaborate with new people who are driven to pursue theirs with me. 
 
-        On this site you will find a portfolio of my applications and projects. Please feel free to contact me if you would like to collaborate!
-      </p>
+          On this site you will find a portfolio of my applications and projects. Please feel free to contact me if you would like to collaborate!
+        </p>
+      </div>
     </div>
   );
 }
