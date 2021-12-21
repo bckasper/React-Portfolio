765a4e344311d4f32239e182402501c8a7f98c04
Ben Kasper <bckasper@gmail.com>
2021-12-21 17:44:59 -0600

    finished my footer content complete with react-icons package used for the linking, github and stack overflow links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { SiLinkedin, SiGithub, SiStackoverflow } from 'react-icons/si'
+
 
 const Footer = () => {
     return(
-        <div>
-            <div className='bg-dark py-3 w-auto'>
-                <p className="text-white">This is my footer</p>
-            </div>
+        <div className='icons bg-dark py-3 container-fluid d-flex justify-content-center text-light'>
+            
+            <a href='https://www.linkedin.com/in/bckasper' target={"_blank"} className='icons'><SiLinkedin/></a>
+            
+            <a href='https://www.github.com/bckasper' target={"_blank"} className='icons'><SiGithub/></a>
+            
+            <a href='https://stackoverflow.com/users/16180108/ben-kasper' target={"_blank"} className='icons'><SiStackoverflow/></a>
+             
         </div>
     )
 }
 
 export default Footer
+
